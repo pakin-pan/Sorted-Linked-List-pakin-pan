3ab9e47491ecfd396232bda102bd2b1af61c7751
pakin-pan <132637361+pakin-pan@users.noreply.github.com>
2023-06-30 03:59:34 +0000

    Initial commit

---
+++ replit.nix
@@ -0,0 +1,21 @@
+{ pkgs }: {
+	deps = [
+		pkgs.gnumake42
+  pkgs.libgccjit
+  pkgs.gnumake42
+  pkgs.gnumake
+  pkgs.gnumake42
+  pkgs.gnumake42
+  pkgs.gnumake42
+  pkgs.gnumake42
+  pkgs.gnumake42
+  pkgs.gnumake42
+  pkgs.gnumake42
+  pkgs.gnumake42
+  pkgs.make run
+  pkgs.clang_12
+		pkgs.ccls
+		pkgs.gdb
+		pkgs.gnumake
+	];
+}
